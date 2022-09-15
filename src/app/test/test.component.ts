bf88b8cfab1fbfd9cb135b1fd52bc543f66f4b14
N-HaoTien <nht.it19@gmail.com>
2022-09-15 13:49:06 +0700

    Data Binding - Events binding - ngModel connect between View and ComponentTest

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit,OnChanges,OnDestroy,AfterViewInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit,AfterViewInit,OnChanges,OnDestroy {
+
+  constructor() { }
+  public Name : string | undefined;
+  public Name2 : string | undefined;
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy')
+
+  }
+  resetName(){
+    this.Name ='';
+  }
+  onKeyDown(){
+    this.Name ='';
+  }
+  onClick(input:any){
+    this.Name = input;
+  }getName2(input :any){
+    this.Name2 = input;
+  }
+  getName(input :any){
+    this.Name = input;
+  }
+  ngOnChanges(_changes: SimpleChanges): void {
+    console.log('ngOnChanges')
+  }
+
+  
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit')
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit')
+  }
+  
+}
